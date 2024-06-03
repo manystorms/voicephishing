ab847492c74e69f2613fde2df3dcf5a1721aec43
manystorms <kimhayul2007@naver.com>
2024-06-03 18:59:29 +0900

    파이썬에서 wav 파일 읽기 성공

--- python/test.py
+++ python/test.py
@@ -1,5 +1,8 @@
 import os
+import sys
 from time import sleep
+
+from op import add
 
 result_filename = os.getenv("RESULT_FILENAME")
 wav_filename = os.getenv("WAV_FILENAME")
@@ -10,11 +13,13 @@ if not result_filename:
     exit(1)
 
 # WAV 파일이 존재하고 확장자가 .wav인지 확인
-if os.path.exists(wav_filename) and wav_filename.lower().endswith('.wav'):
-    with open(wav_filename, 'rb') as file:
-            res = "1"
-else:
-    res = "0"
+#if os.path.exists(wav_filename) and wav_filename.lower().endswith('.wav'):
+#    with open(wav_filename, 'rb') as file:
+#            res = "1"
+#else:
+#    res = "0"
+
+res = str(123)
 
 with open(result_filename, "w") as f:
     f.write(res)
