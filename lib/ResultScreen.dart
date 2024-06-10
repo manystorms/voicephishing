8bdb70f7769177792971800ded998dbb8a0a47e4
manystorms <kimhayul2007@naver.com>
2024-06-10 19:00:16 +0900

    asdf

--- lib/ResultScreen.dart
+++ lib/ResultScreen.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:voicephishing/model/get_FilePath.dart';
+import 'package:voicephishing/model/SpeechToText.dart';
 
-class ResultScreen extends StatelessWidget{
+class ResultScreen extends StatefulWidget {
   final AudioFile audioFile;
-
   ResultScreen({required this.audioFile});
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen>{
+  String AudioText = "변환중입니다";
+  Future<void> _getAudioText() async {
+    AudioText = await SpeechToText(widget.audioFile.Path);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _getAudioText();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(audioFile.Name),
+        title: Text(widget.audioFile.Name),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text("Path: ${audioFile.Path}", style: TextStyle(fontSize: 18)),
-            Text("Date: ${audioFile.Date.toString()}", style: TextStyle(fontSize: 18)),
+            Text("Path: ${widget.audioFile.ShowingDate}", style: TextStyle(fontSize: 18)),
+            Text(AudioText, style: TextStyle(fontSize: 18)),
           ],
         ),
       ),
---
+++ lib/model/SpeechToText.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:vosk_flutter_2/vosk_flutter_2.dart';
+
+Future<String> SpeechToText(String audioFilePath) async {
+  final vosk = VoskFlutterPlugin.instance();
+  final ModelPath = await ModelLoader()
+      .loadFromAssets('assets/vosk-model-small-ko-0.22.zip');
+  final model = await vosk.createModel(ModelPath);
+
+  final recognizer = await vosk.createRecognizer(
+    model: model,
+    sampleRate: 16000,
+  );
+
+  File audioFile = File(audioFilePath);
+  Uint8List audioBytes = await audioFile.readAsBytes();
+
+  String result = "";
+
+  int pos = 0;
+  int chunkSize = 20000; // 처리할 데이터 덩어리의 크기를 정의합니다.
+  while (pos + chunkSize < audioBytes.length) {
+    bool isFinal = await recognizer.acceptWaveformBytes(
+        audioBytes.sublist(pos, pos + chunkSize));
+    if (isFinal) {
+      final res = await recognizer.getResult();
+      result += res;
+    }
+    pos += chunkSize;
+  }
+
+  // 마지막 덩어리의 데이터를 처리합니다.
+  bool isFinal = await recognizer.acceptWaveformBytes(
+      audioBytes.sublist(pos, audioBytes.length));
+  if (isFinal) {
+    final res = await recognizer.getResult();
+    result += res;
+  } else {
+    final res = await recognizer.getFinalResult();
+    result += res;
+  }
+
+  // 인식 결과를 반환합니다.
+  return result;
+}
