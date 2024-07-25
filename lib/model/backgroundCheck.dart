bad3a9266d02895acb707ebe5588189975e2b5d1
eng <ruddbs2817@gmail.com>
2024-07-26 02:58:15 +0900

    백그라운드 성공

--- lib/model/backgroundCheck.dart
+++ lib/model/backgroundCheck.dart
@@ -6,6 +6,8 @@ import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:voicephishing/model/alert.dart';
 import 'package:voicephishing/model/get_FilePath.dart';
+import 'package:voicephishing/model/SpeechToText.dart';
+import 'package:voicephishing/model/AnalyzeText.dart';
 
 void startBackgroundService() {
   final service = FlutterBackgroundService();
@@ -68,33 +70,79 @@ void onStart(ServiceInstance service) async {
 
   service.on("start").listen((event) {});
 
-  bool backGroundWorking = false;
-
   Timer.periodic(const Duration(seconds: 1), (timer) async {
-    if(backGroundWorking == false) {
-      backGroundWorking = true;
+    if(backgroundWorking == false) {
+      backgroundWorking = true;
       socket.emit("event-name", "your-message");
 
-      final lastCheckTime = await getLastCheckTime();
-      print('a');
+      backgroundProcess();
 
-      List<AudioFile> AudioFileList = [];
-      ManageFilePath a = ManageFilePath();
-      AudioFileList = await a.getFileList(true);
-
-      for (AudioFile file in AudioFileList) {
-        if(lastCheckTime.isBefore(file.Date)) {
-          showPushAlarm('확인중', '통화가 끝난 것이 감지되었습니다: ${file.Name}');
-          print('aa: ${file.Name}');
-        }
-      }
-      print(lastCheckTime);
-
-      storeLastCheckTime();
-      backGroundWorking = false;
+      backgroundWorking = false;
     }
   });
 }
+
+bool backgroundWorking = false;
+
+void backgroundProcess() async{
+  final lastCheckTime = await getLastCheckTime();
+  storeLastCheckTime();
+
+  List<AudioFile> AudioFileList = [];
+  List<String> audioSentences = [];
+  List<double> audioVoicephishingCheck = [];
+  double TotalVoicephishingCheck = 0;
+  ManageFilePath a = ManageFilePath();
+  AudioFileList = await a.getFileList(true);
+
+  for (AudioFile file in AudioFileList) {
+    if(lastCheckTime.isBefore(file.Date)) {
+      showPushAlarm('확인중', '통화가 끝난 것이 감지되었습니다: ${file.Name}');
+
+      getAudioText speechToTextClass = getAudioText();
+      final audioText = await speechToTextClass.recognizeAudioFile(file.Path);
+
+      audioSentences = audioText.split(RegExp(r'\.\s+'));
+      audioVoicephishingCheck.clear();
+
+      final classifier = TextClassifier();
+
+      await classifier.initialize();
+
+      try {
+        int TotalAudioTextLen = 0;
+        double TotalRes = 0;
+        for (String text in audioSentences) {
+          double result = await classifier.classifyText(text);
+          audioVoicephishingCheck.add(result);
+          print("문자: $text, 예측 결과: $result");
+
+          TotalAudioTextLen = TotalAudioTextLen+text.length;
+          TotalRes = TotalRes+text.length*result;
+        }
+        TotalVoicephishingCheck = TotalRes/TotalAudioTextLen.toDouble();
+
+        if(TotalVoicephishingCheck >= 70) {
+          showPushAlarm(
+              '보이스피싱 의심',
+              '해당 통화가 보이스피싱일 가능성이 $TotalVoicephishingCheck% 입니다(파일명: ${file.Name})'
+          );
+        }else{
+          showPushAlarm(
+              '보이스피싱 의심',
+              '해당 통화는 보이스피싱이 아닙니다(파일명: ${file.Name})'
+          );
+        }
+      } catch (e) {
+        print('분류 중 오류가 발생했습니다: $e');
+      } finally {
+        classifier.dispose();
+      }
+    }
+  }
+  print("백그라운드 실행: $lastCheckTime");
+}
+
 
 void storeLastCheckTime() async{
   SharedPreferences pref = await SharedPreferences.getInstance();
